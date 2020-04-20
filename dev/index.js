90f1c5c7f6db8c009de3833bf75d569c557880a8
Eitan Alkaslassy <eitanalka@gmail.com>
2020-04-19 21:26:27 -0400

    added peerjs server

--- dev/index.js
+++ dev/index.js
@@ -3,6 +3,8 @@ import '@babel/polyfill';
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import { PeerServer } from 'peer';
+
 import router from './routes';
 
 const port = process.env.PORT || 3001;
@@ -19,6 +21,10 @@ app.use(express.json());
 
 // all api requests are expected to start with /api
 app.use('/api', router);
+
+PeerServer({ port: 9000, path: '/peerserver' }, () => {
+  console.log('Peer server listening on port 9000');
+});
 
 app.listen(port, () => {
   console.log('Server listening on port 3001');

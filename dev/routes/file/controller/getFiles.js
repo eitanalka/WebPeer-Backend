c52f274fb7c4722d0fe4342ae4f7b6b852be825b
Eitan Alkaslassy <eitanalka@gmail.com>
2020-04-21 22:35:13 -0400

    getFiles returns array of file objects

--- dev/routes/file/controller/getFiles.js
+++ dev/routes/file/controller/getFiles.js
@@ -1,6 +1,6 @@
 const getFiles = async (req, res) => {
   const files = req.app.get('files');
-  return res.status(200).json({ files: Object.values(files) });
+  return res.status(200).json(Object.values(files));
 };
 
 export default getFiles;
